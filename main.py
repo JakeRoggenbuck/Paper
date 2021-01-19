c2ab3f1ca9d602604941a03cbbdbaa260df1784f
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-01-19 12:52:17 -0800

    Add vars and parser

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from enum import Enum
 import shlex
 
 
+class Var:
+    def __init__(self, data):
+        self.data = data
+
+
+class String(Var):
+    def __init__(self, data):
+        super().__init__(data)
+
+
+class Int(Var):
+    def __init__(self, data):
+        super().__init__(data)
+
+
 class TokenType(Enum):
     NONE = 0
     PRINT = 1
@@ -42,7 +57,7 @@ class Tokens:
         return f"tokens[{self.index}] = TokenType({self.get()})"
 
 
-class Parser:
+class Tokenizer:
     def __init__(self):
         self.items = []
         self.tokens = Tokens()
@@ -79,37 +94,69 @@ class Parser:
 
         return token
 
-    def parse_each_line(self):
+    def tokenize_each_line(self):
         for line in self.lines:
             items = shlex.shlex(line, punctuation_chars=True)
-            self.parse_each_item(items)
+            self.tokenize_each_item(items)
 
-    def parse_each_item(self, items: list):
+    def tokenize_each_item(self, items: list):
         for item in items:
             self.items.append(item)
             token = self.get_token_type(item)
             self.tokens.add(token)
 
-    def parse(self):
-        self.parse_each_line()
+    def tokenize(self):
+        self.tokenize_each_line()
 
 
-class Runner:
+class Parser:
     def __init__(self, tokens: list, items: list):
         self.tokens = tokens
         self.items = items
+        self.vars_in_mem = {}
 
-    def run(self):
+    def get_data_from_string(self, string: str):
+        return string[1:-1]
+
+    def parse_item(self):
+        # Starts with print
+        if self.tokens.get().token == TokenType.PRINT:
+            if self.tokens.next().get().token == TokenType.STRING_DATA:
+                print(self.get_data_from_string(self.items[self.tokens.index + 1]))
+
+        # Starts with string
+        if self.tokens.get().token == TokenType.STRING:
+            if self.tokens.next().get().token == TokenType.VAR_NAME:
+                name = self.items[self.tokens.index + 1][1:]
+                if self.tokens.next(2).get().token == TokenType.EQUAL:
+
+                    # Set a raw string
+                    if self.tokens.next(3).get().token == TokenType.STRING_DATA:
+                        self.vars_in_mem[name] = String(self.items[self.tokens.index + 3])
+
+                    # Get value from input, set as string
+                    elif self.tokens.next(3).get().token == TokenType.INPUT:
+                        if self.tokens.next(4).get().token == TokenType.STRING_DATA:
+                            get_input = input(
+                                self.get_data_from_string(self.items[self.tokens.index + 4])
+                            )
+                            self.vars_in_mem[name] = String(get_input)
+
+        self.tokens.index += 1
+
+    def parse(self):
         index = 0
         while index < len(self.tokens.tokens):
-            token = self.tokens.tokens[index].token
-            data = self.items[index]
-            print(f"{token}:\t{data}")
+            item = self.items[index]
+            self.parse_item()
+            # print(self.tokens.tokens[index].token, item)
             index += 1
 
 
 if __name__ == "__main__":
-    parser = Parser()
+    tokenizer = Tokenizer()
+    tokenizer.tokenize()
+
+    parser = Parser(tokenizer.tokens, tokenizer.items)
     parser.parse()
-    runner = Runner(parser.tokens, parser.items)
-    runner.run()
+    print(parser.vars_in_mem)
